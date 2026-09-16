7cdf3354bbab488f178dc281b95bc212488cb1e8
Rohan <gitlink7@gmail.com>
2026-09-16 17:12:40 +0530

    found not found

--- notfound.c
+++ notfound.c
@@ -2,12 +2,14 @@
 int main()
 {
     int a[5]={23,87,20,86};
-    int key=86;
+    int key=20,found = 0;
     for(int i=0;i<5;i++){
         if(a[i]==key){
-            printf("key found : %d",i);
+           found = 1;
             break;
         }
     }
+        if(found==0) printf("not found");
+        else printf("found");
     return 0;
 }
